2f8d9b83d1b37d24c5e4e66a91b277832a5ac5f0
dima192 <68648827+dima192@users.noreply.github.com>
2020-07-22 16:49:12 +0300

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
+        int arrll = arr.length;
+        for (int i = 0; i < arrll; i++) {
+            if (arr[i] == 1) arr[i] = 0;
+            else arr[i] = 1;
+        }
+        System.out.println("Task 1:");
+            System.out.println(Arrays.toString(arr));
+
+    }
+    
+}
