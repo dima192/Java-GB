2e0ff04edaadc805871f56b98c5e2b702966d35b
dima192 <68648827+dima192@users.noreply.github.com>
2020-07-26 17:03:11 +0300

    Add files via upload

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,19 +1,50 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main {
-    /*
-    public static void main(String[] args) {
-
-        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
-        int arrll = arr.length;
-        for (int i = 0; i < arrll; i++) {
-            if (arr[i] == 1) arr[i] = 0;
-            else arr[i] = 1;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        int prog, user;
+        do
+        {
+            prog = (int)(Math.random() * 10);
+            System.out.print("\nprog: " + prog);  /* Для проверки работы программы */
         }
-        System.out.println("Task 1:");
-            System.out.println(Arrays.toString(arr));
-
+        while (prog == 0);
+        System.out.println("\nВыбрано число от 1 до 10, отгадайте его.");
+        System.out.print("Введите ваше число: ");
+        Scanner input = new Scanner(System.in);
+        if( input.hasNextInt() )
+        {
+            do {
+                user = input.nextInt();
+                if(user == prog) {
+                    System.out.println("Вы угадали!");
+                } else {
+                    if (user >= -10 && user <= 10)
+                    {
+                        System.out.print("Вы не угадали! ");
+                        if( prog < user )
+                        {
+                            System.out.println("Ваше число больше.");
+                        } else {
+                            System.out.println("Ваше число меньше.");
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Ваше число вообще не из нужного отрезка!");
+                    }
+                }
+                i++;
+            }
+            while( i < 3 );
+            System.out.println("Вы не угадали количество попыток исчерпано! ");
+        }
+        else
+        {
+            System.out.println("Ошибка. Вы не ввели целое число!");
+        }
     }
-    /*
-
 }
